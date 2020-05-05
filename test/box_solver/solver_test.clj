ebf8f0fb8ffdb13fdd74604dc487b476efbf93d3
Lynch the Lionheart <david.w.lynch-jr@boeing.com>
2020-05-05 09:51:01 -0400

    merge

---
+++ test/box_solver/solver_test.clj
@@ -0,0 +1,14 @@
+(ns box-solver.solver-test
+  (:require [clojure.test :refer :all]
+            [box-solver.solver :refer :all]))
+
+(deftest moving-pieces
+  (testing "robot can step on exit")
+                                        ;
+  (testing "robot can pick up gear")
+                                        ;
+  (testing "robot can push crate over exit")
+                                        ;
+  (testing "robot can open force-field with crate on switch")
+                                        ;
+  )
